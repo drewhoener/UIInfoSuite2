93c99fafa9d0b407bfdce24c85dd44e0aa5b6678
Drew Hoener <drewhoener@gmail.com>
2023-10-30 18:49:19 -0400

    Refactor ModOptionsElement to use the new OptionStringWrapper class

--- UIInfoSuite2/Options/ModOptionsElement.cs
+++ UIInfoSuite2/Options/ModOptionsElement.cs
@@ -13,14 +13,19 @@ namespace UIInfoSuite2.Options
         protected const int DefaultPixelSize = 9;
 
         private Rectangle _bounds;
-        private string _label;
+        protected readonly OptionStringWrapper Label;
         private int _whichOption;
 
         protected readonly ModOptionsElement _parent;
 
         public Rectangle Bounds => _bounds;
 
-        public ModOptionsElement(string label, int whichOption = -1, ModOptionsElement parent = null)
+        public ModOptionsElement(string label, int whichOption = -1, ModOptionsElement parent = null) : this(
+            new OptionStringWrapper(label, false), whichOption, parent)
+        {
+        }
+
+        public ModOptionsElement(OptionStringWrapper label, int whichOption = -1, ModOptionsElement parent = null)
         {
             int x = DefaultX * Game1.pixelZoom;
             int y = DefaultY * Game1.pixelZoom;
@@ -31,7 +36,7 @@ namespace UIInfoSuite2.Options
                 x += DefaultX * 2 * Game1.pixelZoom;
 
             _bounds = new Rectangle(x, y, width, height);
-            _label = label;
+            Label = label;
             _whichOption = whichOption;
 
             _parent = parent;
@@ -39,34 +44,31 @@ namespace UIInfoSuite2.Options
 
         public virtual void ReceiveLeftClick(int x, int y)
         {
-
         }
 
         public virtual void LeftClickHeld(int x, int y)
         {
-
         }
 
         public virtual void LeftClickReleased(int x, int y)
         {
-
         }
 
         public virtual void ReceiveKeyPress(Keys key)
         {
-
         }
 
         public virtual void Draw(SpriteBatch batch, int slotX, int slotY)
         {
             if (_whichOption < 0)
             {
-                SpriteText.drawString(batch, _label, slotX + _bounds.X, slotY + _bounds.Y + Game1.pixelZoom * 3, 999, -1, 999, 1, 0.1f);
+                SpriteText.drawString(batch, Label.ToString(), slotX + _bounds.X,
+                    slotY + _bounds.Y + Game1.pixelZoom * 3, 999, -1, 999, 1, 0.1f);
             }
             else
             {
                 Utility.drawTextWithShadow(batch,
-                    _label,
+                    Label.ToString(),
                     Game1.dialogueFont,
                     new Vector2(slotX + _bounds.X + _bounds.Width + Game1.pixelZoom * 2, slotY + _bounds.Y),
                     Game1.textColor,
