c1d1edd798d504702cb48857de1767a5ee16e967
Drew Hoener <drewhoener@gmail.com>
2023-10-30 18:48:53 -0400

    Add intermediate class ModOptionsValueElement to allow for more configurable option types that contain some value.

---
+++ UIInfoSuite2/Options/ModOptionsValueElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UIInfoSuite2.Options
+{
+    public abstract class ModOptionsValueElement<T> : ModOptionsElement, IOptionValue<T>
+    {
+        protected readonly Action<string, T>? OnValueUpdate;
+
+        protected ModOptionsValueElement(Action<string, T>? onValueUpdate, OptionStringWrapper label, int whichOption = -1, ModOptionsElement parent = null) : base(label, whichOption, parent)
+        {
+            OnValueUpdate = onValueUpdate;
+        }
+
+        public abstract T GetValue();
+        public abstract void SetValue(T newValue);
+    }
+}
