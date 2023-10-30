8c4080f694762430c92a65fcfe13bc94873bd42f
Drew Hoener <drewhoener@gmail.com>
2023-10-30 19:33:40 -0400

    Updated ShopHarvestPrices.

--- UIInfoSuite2/Options/ModOptionsPageHandler.cs
+++ UIInfoSuite2/Options/ModOptionsPageHandler.cs
@@ -72,7 +72,7 @@ namespace UIInfoSuite2.Options
             var showScarecrowAndSprinklerRange = new ShowItemEffectRanges(helper);
             var experienceBar = new ExperienceBar(helper);
             var showItemHoverInformation = new ShowItemHoverInformation(helper);
-            var shopHarvestPrices = new ShopHarvestPrices(helper);
+            var shopHarvestPrices = new ShopHarvestPrices(helper, options);
             var showQueenOfSauceIcon = new ShowQueenOfSauceIcon(helper);
             var showTravelingMerchant = new ShowTravelingMerchant(helper);
             var showRainyDayIcon = new ShowRainyDayIcon(helper);
@@ -134,7 +134,6 @@ namespace UIInfoSuite2.Options
             _optionsElements.Add(travellingMerchantIcon);
             _optionsElements.Add(new ModOptionsCheckbox(_helper.SafeGetString(nameof(options.HideMerchantWhenVisited)), whichOption++, showTravelingMerchant.ToggleHideWhenVisitedOption, () => options.HideMerchantWhenVisited, v => options.HideMerchantWhenVisited = v, travellingMerchantIcon));
             _optionsElements.Add(new ModOptionsCheckbox(_helper.SafeGetString(nameof(options.ShowRainyDay)), whichOption++, showRainyDayIcon.ToggleOption, () => options.ShowRainyDay, v => options.ShowRainyDay = v));
-            _optionsElements.Add(new ModOptionsCheckbox(_helper.SafeGetString(nameof(options.ShowHarvestPricesInShop)), whichOption++, shopHarvestPrices.ToggleOption, () => options.ShowHarvestPricesInShop, v => options.ShowHarvestPricesInShop = v));
             _optionsElements.Add(new ModOptionsCheckbox(_helper.SafeGetString(nameof(options.ShowWhenNewRecipesAreAvailable)), whichOption++, showQueenOfSauceIcon.ToggleOption, () => options.ShowWhenNewRecipesAreAvailable, v => options.ShowWhenNewRecipesAreAvailable = v));
             _optionsElements.Add(new ModOptionsCheckbox(_helper.SafeGetString(nameof(options.ShowToolUpgradeStatus)), whichOption++, showToolUpgradeStatus.ToggleOption, () => options.ShowToolUpgradeStatus, v => options.ShowToolUpgradeStatus = v));
             _optionsElements.Add(new ModOptionsCheckbox(_helper.SafeGetString(nameof(options.ShowRobinBuildingStatusIcon)), whichOption++, showRobinBuildingStatusIcon.ToggleOption, () => options.ShowRobinBuildingStatusIcon, v => options.ShowRobinBuildingStatusIcon = v));
--- UIInfoSuite2/UIElements/ShopHarvestPrices.cs
+++ UIInfoSuite2/UIElements/ShopHarvestPrices.cs
@@ -4,126 +4,128 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
-using System;
 using UIInfoSuite2.Infrastructure;
 using UIInfoSuite2.Infrastructure.Extensions;
+using UIInfoSuite2.Options;
+using UIInfoSuite2.UIElements.Base;
 
 namespace UIInfoSuite2.UIElements
 {
-    internal class ShopHarvestPrices : IDisposable
+    internal class ShopHarvestPrices : UIElementBase
     {
-        private readonly IModHelper _helper;
-
-        public ShopHarvestPrices(IModHelper helper)
+        public ShopHarvestPrices(IModHelper helper, ModOptions options) : base(helper, options)
         {
-            _helper = helper;
         }
 
-        public void ToggleOption(bool shopHarvestPrices)
+        protected override bool ShouldEnable()
         {
-            _helper.Events.Display.RenderedActiveMenu -= OnRenderedActiveMenu;
-
-            if (shopHarvestPrices)
-            {
-                _helper.Events.Display.RenderedActiveMenu += OnRenderedActiveMenu;
-            }
+            return Options.ShowHarvestPricesInShop;
         }
 
-        public void Dispose()
+        protected override void RegisterEvents()
         {
-            ToggleOption(false);
+            Helper.Events.Display.RenderedActiveMenu += OnRenderedActiveMenu;
         }
 
-        /// <summary>When a menu is open (<see cref="Game1.activeClickableMenu"/> isn't null), raised after that menu is drawn to the sprite batch but before it's rendered to the screen.</summary>
+        protected override void UnregisterEvents()
+        {
+            Helper.Events.Display.RenderedActiveMenu -= OnRenderedActiveMenu;
+        }
+
+        protected override void SetUpOptions()
+        {
+            OptionElements.Add(
+                new ModOptionsCheckbox(
+                    new OptionStringWrapper(nameof(Options.ShowHarvestPricesInShop)),
+                    1,
+                    (s, b) => OnOptionsChanged(s, b),
+                    () => Options.ShowHarvestPricesInShop,
+                    v => Options.ShowHarvestPricesInShop = v
+                )
+            );
+        }
+
+        /// <summary>
+        ///     When a menu is open (<see cref="Game1.activeClickableMenu" /> isn't null), raised after that menu is drawn to
+        ///     the sprite batch but before it's rendered to the screen.
+        /// </summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
-        private void OnRenderedActiveMenu(object sender, RenderedActiveMenuEventArgs e)
+        private void OnRenderedActiveMenu(object? sender, RenderedActiveMenuEventArgs e)
         {
-            if (!(Game1.activeClickableMenu is ShopMenu menu)) return;
-            if (!(menu.hoveredItem is Item hoverItem)) return;
+            if (Game1.activeClickableMenu is not ShopMenu menu)
+            {
+                return;
+            }
+
+            if (menu.hoveredItem is not Item hoverItem)
+            {
+                return;
+            }
 
             // draw shop harvest prices
             int value = Tools.GetHarvestPrice(hoverItem);
 
-            if (value > 0)
+            if (value <= 0)
             {
-                int xPosition = menu.xPositionOnScreen - 30;
-                int yPosition = menu.yPositionOnScreen + 580;
-                IClickableMenu.drawTextureBox(
-                    Game1.spriteBatch,
-                    xPosition + 20,
-                    yPosition - 52,
-                    264,
-                    108,
-                    Color.White);
-                // Title "Harvest Price"
-                string textToRender = _helper.SafeGetString(LanguageKeys.HarvestPrice);
-                Game1.spriteBatch.DrawString(
-                    Game1.dialogueFont,
-                    textToRender,
-                    new Vector2(xPosition + 30, yPosition - 38),
-                    Color.Black * 0.2f);
-                Game1.spriteBatch.DrawString(
-                    Game1.dialogueFont,
-                    textToRender,
-                    new Vector2(xPosition + 32, yPosition - 40),
-                    Color.Black * 0.8f);
-                // Tree Icon
-                xPosition += 80;
-                Game1.spriteBatch.Draw(
-                    Game1.mouseCursors,
-                    new Vector2(xPosition, yPosition),
-                    new Rectangle(60, 428, 10, 10),
-                    Color.White,
-                    0,
-                    Vector2.Zero,
-                    Game1.pixelZoom,
-                    SpriteEffects.None,
-                    0.85f);
-                //  Coin
-                Game1.spriteBatch.Draw(
-                    Game1.debrisSpriteSheet,
-                    new Vector2(xPosition + 32, yPosition + 10),
-                    Game1.getSourceRectForStandardTileSheet(Game1.debrisSpriteSheet, 8, 16, 16),
-                    Color.White,
-                    0,
-                    new Vector2(8, 8),
-                    4,
-                    SpriteEffects.None,
-                    0.95f);
-                // Price
-                Game1.spriteBatch.DrawString(
-                    Game1.dialogueFont,
-                    value.ToString(),
-                    new Vector2(xPosition + 50, yPosition + 6),
-                    Color.Black * 0.2f);
-                Game1.spriteBatch.DrawString(
-                    Game1.dialogueFont,
-                    value.ToString(),
-                    new Vector2(xPosition + 52, yPosition + 4),
-                    Color.Black * 0.8f);
-                /*
-				 * I have no Idea why this is here...
-				 * As far as I can see it only overrides the existing Tooltip with a price that is 500 coins higher?
-				 *
-                string hoverText = _helper.Reflection.GetField<string>(menu, "hoverText").GetValue();
-                string hoverTitle = _helper.Reflection.GetField<string>(menu, "boldTitleText").GetValue();
-                IReflectedMethod getHoveredItemExtraItemIndex = _helper.Reflection.GetMethod(menu, "getHoveredItemExtraItemIndex");
-                IReflectedMethod getHoveredItemExtraItemAmount = _helper.Reflection.GetMethod(menu, "getHoveredItemExtraItemAmount");
-                IClickableMenu.drawToolTip(
-                    Game1.spriteBatch,
-                    hoverText,
-                    hoverTitle,
-                    hoverItem,
-                    menu.heldItem != null,
-                    -1,
-                    menu.currency,
-                    getHoveredItemExtraItemIndex.Invoke<int>(new object[0]),
-                    getHoveredItemExtraItemAmount.Invoke<int>(new object[0]),
-                    null,
-                    menu.hoverPrice);
-				*/
+                return;
             }
+
+            int xPosition = menu.xPositionOnScreen - 30;
+            int yPosition = menu.yPositionOnScreen + 580;
+            IClickableMenu.drawTextureBox(Game1.spriteBatch, xPosition + 20, yPosition - 52, 264, 108, Color.White);
+            // Title "Harvest Price"
+            string textToRender = Helper.SafeGetString(LanguageKeys.HarvestPrice);
+            Game1.spriteBatch.DrawString(
+                Game1.dialogueFont,
+                textToRender,
+                new Vector2(xPosition + 30, yPosition - 38),
+                Color.Black * 0.2f
+            );
+            Game1.spriteBatch.DrawString(
+                Game1.dialogueFont,
+                textToRender,
+                new Vector2(xPosition + 32, yPosition - 40),
+                Color.Black * 0.8f
+            );
+            // Tree Icon
+            xPosition += 80;
+            Game1.spriteBatch.Draw(
+                Game1.mouseCursors,
+                new Vector2(xPosition, yPosition),
+                new Rectangle(60, 428, 10, 10),
+                Color.White,
+                0,
+                Vector2.Zero,
+                Game1.pixelZoom,
+                SpriteEffects.None,
+                0.85f
+            );
+            //  Coin
+            Game1.spriteBatch.Draw(
+                Game1.debrisSpriteSheet,
+                new Vector2(xPosition + 32, yPosition + 10),
+                Game1.getSourceRectForStandardTileSheet(Game1.debrisSpriteSheet, 8, 16, 16),
+                Color.White,
+                0,
+                new Vector2(8, 8),
+                4,
+                SpriteEffects.None,
+                0.95f
+            );
+            // Price
+            Game1.spriteBatch.DrawString(
+                Game1.dialogueFont,
+                value.ToString(),
+                new Vector2(xPosition + 50, yPosition + 6),
+                Color.Black * 0.2f
+            );
+            Game1.spriteBatch.DrawString(
+                Game1.dialogueFont,
+                value.ToString(),
+                new Vector2(xPosition + 52, yPosition + 4),
+                Color.Black * 0.8f
+            );
         }
     }
 }
