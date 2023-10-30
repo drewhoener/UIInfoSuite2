89f1a2a0d44f388c73f3cb24a5517ec820731c23
Drew Hoener <drewhoener@gmail.com>
2023-10-30 19:00:41 -0400

    Added UI Base elements that can be extended to reuse more code. UI Elements will now manage their own options. Extended ClickableTextureComponent to use common logic for hovering

---
+++ UIInfoSuite2/UIElements/Base/UIElementBase.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using StardewModdingAPI;
+using UIInfoSuite2.Options;
+
+namespace UIInfoSuite2.UIElements.Base
+{
+    internal abstract class UIElementBase : IDisposable
+    {
+        protected readonly IModHelper Helper;
+        protected readonly List<ModOptionsElement> OptionElements = new();
+        protected readonly ModOptions Options;
+
+        private bool _enabled;
+        protected static IMonitor Logger => ModEntry.MonitorObject;
+
+        #region Lifecycle
+
+        protected UIElementBase(IModHelper helper, ModOptions options)
+        {
+            Helper = helper;
+            Options = options;
+            _Init();
+        }
+
+        private void _Init()
+        {
+            Init();
+            SetUpOptions();
+            Reload();
+        }
+
+        protected virtual void Init()
+        {
+        }
+
+        public virtual void Dispose()
+        {
+            Disable();
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region UI Element Enable / Disable
+
+        protected abstract bool ShouldEnable();
+
+        protected virtual void OnEnable()
+        {
+        }
+
+        protected virtual void OnDisable()
+        {
+        }
+
+        public void Enable()
+        {
+            if (_enabled)
+            {
+                return;
+            }
+
+            OnEnable();
+            RegisterEvents();
+            _RegisterInternalEvents();
+            _enabled = true;
+        }
+
+        public void Disable()
+        {
+            if (!_enabled)
+            {
+                return;
+            }
+
+            OnDisable();
+            _UnregisterInternalEvents();
+            UnregisterEvents();
+            _enabled = false;
+        }
+
+        public void Reload()
+        {
+            Disable();
+            if (ShouldEnable())
+            {
+                Enable();
+            }
+        }
+
+        #endregion
+
+        #region Options
+
+        protected abstract void SetUpOptions();
+
+        protected virtual void OnOptionsChanged(string? _, dynamic? __)
+        {
+            Reload();
+        }
+
+        public IEnumerable<ModOptionsElement> GetOptionsList()
+        {
+            return OptionElements.AsReadOnly();
+        }
+
+        #endregion
+
+        #region Events
+
+        protected abstract void RegisterEvents();
+
+        protected abstract void UnregisterEvents();
+
+        protected virtual void _RegisterInternalEvents()
+        {
+        }
+
+        protected virtual void _UnregisterInternalEvents()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ UIInfoSuite2/UIElements/Base/UIHudElement.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+using UIInfoSuite2.Infrastructure;
+using UIInfoSuite2.Infrastructure.Extensions;
+using UIInfoSuite2.Options;
+
+namespace UIInfoSuite2.UIElements.Base
+{
+    internal abstract class UIHudElement : UIHoverElement
+    {
+        protected readonly PerScreen<ClickableTextureComponent> Icon = new();
+
+        protected UIHudElement(IModHelper helper, ModOptions options) : base(helper, options)
+        {
+        }
+
+        #region Event Registration
+
+        protected override void _RegisterInternalEvents()
+        {
+            Helper.Events.Display.RenderingHud += Internal_RenderHudIcon;
+            base._RegisterInternalEvents();
+        }
+
+        protected override void _UnregisterInternalEvents()
+        {
+            Helper.Events.Display.RenderingHud -= Internal_RenderHudIcon;
+            base._UnregisterInternalEvents();
+        }
+
+        #endregion
+
+        protected sealed override bool ShouldRenderHoverText()
+        {
+            return ShouldDrawIcon() && Icon.Value.IsHoveredOver();
+        }
+
+        #region Icon Rendering
+
+        /// <summary>
+        ///     Helper handler for rendering the icon on the UI Bar
+        /// </summary>
+        /// <param name="sender">Event sender, nullable</param>
+        /// <param name="e">Event arguments</param>
+        private void Internal_RenderHudIcon(object? sender, RenderingHudEventArgs e)
+        {
+            if (Game1.eventUp)
+            {
+                return;
+            }
+
+            DrawIcon();
+        }
+
+        protected virtual bool ShouldDrawIcon()
+        {
+            return true;
+        }
+
+        protected virtual void MoveIconToPos(Point? point = null)
+        {
+            if (!ShouldDrawIcon())
+            {
+                return;
+            }
+
+            Point iconPos = point ?? IconHandler.Handler.GetNewIconPosition();
+            Icon.Value.setPosition(iconPos.X, iconPos.Y);
+        }
+
+        protected virtual void DrawIcon(bool moveAutomatically = true)
+        {
+            if (moveAutomatically)
+            {
+                MoveIconToPos();
+            }
+
+            if (ShouldDrawIcon())
+            {
+                DrawDelegate();
+            }
+        }
+
+        protected virtual void DrawDelegate()
+        {
+            Icon.Value.draw(Game1.spriteBatch);
+        }
+
+        #endregion
+    }
+}
