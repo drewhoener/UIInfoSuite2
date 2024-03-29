ffbaecd3440bf70f898c2c1e4c5425c53e7960b7
Drew Hoener <drewhoener@gmail.com>
2024-03-29 18:42:20 -0400

    feat: Patch Item class to make descriptionWidth longer if necessary

---
+++ UIInfoSuite2/Patches/ItemPatches.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using HarmonyLib;
+using StardewValley;
+using UIInfoSuite2.Infrastructure;
+
+namespace UIInfoSuite2.Patches;
+
+internal class ItemPatches
+{
+  public static void Apply(Harmony harmony)
+  {
+    const BindingFlags flag = BindingFlags.Instance | BindingFlags.NonPublic;
+    harmony.Patch(
+      typeof(Item).GetMethod("getDescriptionWidth", flag),
+      postfix: new HarmonyMethod(typeof(ItemPatches), nameof(PatchDefaultDescriptionWidth_Postfix))
+    );
+  }
+
+
+  [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Harmony")]
+  private static void PatchDefaultDescriptionWidth_Postfix(ref int __result, Item __instance)
+  {
+    BundleRequiredItem? bundleInfo = BundleHelper.GetBundleItemIfNotDonated(__instance);
+    if (bundleInfo == null)
+    {
+      return;
+    }
+
+    __result = Math.Max(__result, bundleInfo.BannerWidth);
+  }
+}
