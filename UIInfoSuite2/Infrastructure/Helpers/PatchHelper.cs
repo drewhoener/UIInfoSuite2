e2ed88995e855dfeb1c538e3439d2894ac30ff0c
Drew Hoener <drewhoener@gmail.com>
2024-04-10 17:38:10 -0400

    feat: Add patch helper to easily generate an instruction for finding a loc

---
+++ UIInfoSuite2/Infrastructure/Helpers/PatchHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace UIInfoSuite2.Infrastructure.Helpers;
+
+public static class PatchHelper
+{
+  public static CodeMatch CreateLocMatcher(OpCode code, int locIdx)
+  {
+    return new CodeMatch(
+      instruction => instruction.opcode == code &&
+                     instruction.operand is LocalBuilder builder &&
+                     builder.LocalIndex == locIdx
+    );
+  }
+}
