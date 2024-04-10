4cd22f335b0f9d1d9e176fc33f88c4c1a17db1fa
Drew Hoener <drewhoener@gmail.com>
2024-04-09 22:58:06 -0400

    feat: Add Moving Average helper class, provides avg, var, and stddev

---
+++ UIInfoSuite2/Infrastructure/MovingAverage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UIInfoSuite2.Infrastructure;
+
+public class MovingAverage
+{
+  public int Count { get; private set; }
+  public double Avg { get; private set; }
+
+  public double Variance { get; private set; }
+  public double StdDev { get; private set; }
+
+  public void AddValue(int value)
+  {
+    AddValue((double)value);
+  }
+
+  public void AddValue(float value)
+  {
+    AddValue((double)value);
+  }
+
+  public void AddValue(double value)
+  {
+    double oldAvg = Avg;
+    Count++;
+    Avg += (value - Avg) / Count;
+    Variance += (value - oldAvg) * (value - Avg);
+    StdDev = Math.Sqrt(Variance);
+  }
+
+  public void Reset()
+  {
+    ResetTo(0, 0);
+  }
+
+  public void ResetTo(double avg, int count = 1)
+  {
+    Avg = avg;
+    Count = count;
+  }
+}
