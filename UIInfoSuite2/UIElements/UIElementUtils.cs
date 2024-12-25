467826f96b768ecb3d8db12df84c5f25a5a9fc43
Drew Hoener <drewhoener@gmail.com>
2024-12-24 21:31:25 -0500

    Prevent icons from disappearing when in a menu

--- UIInfoSuite2/UIElements/UIElementUtils.cs
+++ UIInfoSuite2/UIElements/UIElementUtils.cs
@@ -9,15 +9,16 @@ public static class UIElementUtils
   public static bool IsRenderingNormally()
   {
     bool[] conditions =
-    {
+    [
       !Game1.game1.takingMapScreenshot,
-      !Game1.eventUp,
+      !Game1.eventUp || Game1.isFestival(),
       !Game1.viewportFreeze,
       !Game1.freezeControls,
       Game1.viewportHold <= 0,
       Game1.displayHUD,
-      Context.IsPlayerFree
-    };
+      Context.IsWorldReady,
+      Game1.currentLocation != null
+    ];
 
     return conditions.All(condition => condition);
   }
