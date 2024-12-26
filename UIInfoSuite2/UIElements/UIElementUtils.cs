3b054234b480f1c1665e59a39bff64db95683485
Drew Hoener <drewhoener@gmail.com>
2024-12-26 01:58:23 -0500

    Change IsRenderingNormally to remove unneeded LINQ expression

--- UIInfoSuite2/UIElements/UIElementUtils.cs
+++ UIInfoSuite2/UIElements/UIElementUtils.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using StardewModdingAPI;
+﻿using StardewModdingAPI;
 using StardewValley;
 
 namespace UIInfoSuite2.UIElements;
@@ -8,18 +7,13 @@ public static class UIElementUtils
 {
   public static bool IsRenderingNormally()
   {
-    bool[] conditions =
-    [
-      !Game1.game1.takingMapScreenshot,
-      !Game1.eventUp || Game1.isFestival(),
-      !Game1.viewportFreeze,
-      !Game1.freezeControls,
-      Game1.viewportHold <= 0,
-      Game1.displayHUD,
-      Context.IsWorldReady,
-      Game1.currentLocation != null
-    ];
-
-    return conditions.All(condition => condition);
+    return !Game1.game1.takingMapScreenshot &&
+           (!Game1.eventUp || Game1.isFestival()) &&
+           !Game1.viewportFreeze &&
+           !Game1.freezeControls &&
+           Game1.viewportHold <= 0 &&
+           Game1.displayHUD &&
+           Context.IsWorldReady &&
+           Game1.currentLocation != null;
   }
 }
