58ed7154412117757be3873459109a39d060b458
Drew Hoener <drewhoener@gmail.com>
2025-01-06 17:28:41 -0500

    Allow changing dimensions for scalingdimensions

--- UIInfoSuite2/Infrastructure/Models/ScalingDimensions.cs
+++ UIInfoSuite2/Infrastructure/Models/ScalingDimensions.cs
@@ -10,37 +10,63 @@ public enum PrimaryDimension
 
 public class ScalingDimensions
 {
-  private readonly PrimaryDimension _primaryDimension;
-
-  private readonly Rectangle _sourceDimensions;
+  private readonly float _finalSize;
+  private PrimaryDimension _primaryDimension;
+  private Rectangle _sourceDimensions;
 
   public ScalingDimensions(Rectangle sourceBounds, float finalSize, PrimaryDimension primaryDimension)
   {
     _sourceDimensions = sourceBounds;
     _primaryDimension = primaryDimension;
+    _finalSize = finalSize;
 
-    if (_primaryDimension == PrimaryDimension.Height)
+    RecalculateScaling();
+  }
+
+  public Rectangle SourceDimensions
+  {
+    get => _sourceDimensions;
+    set
     {
-      Height = finalSize;
-      ScaleFactor = Height / _sourceDimensions.Height;
-      Width = _sourceDimensions.Width * ScaleFactor;
+      _sourceDimensions = value;
+      RecalculateScaling();
     }
-    else
+  }
+
+  public PrimaryDimension PrimaryDimension
+  {
+    get => _primaryDimension;
+    set
     {
-      Width = finalSize;
-      ScaleFactor = Width / _sourceDimensions.Width;
-      Height = _sourceDimensions.Height * ScaleFactor;
+      _primaryDimension = value;
+      RecalculateScaling();
     }
   }
 
   public (float, float) Bounds => (Width, Height);
 
-  public float Width { get; }
+  public float Width { get; private set; }
 
-  public float Height { get; }
+  public float Height { get; private set; }
 
   public int WidthInt => (int)Width;
 
   public int HeightInt => (int)Height;
-  public float ScaleFactor { get; }
+  public float ScaleFactor { get; private set; }
+
+  private void RecalculateScaling()
+  {
+    if (_primaryDimension == PrimaryDimension.Height)
+    {
+      Height = _finalSize;
+      ScaleFactor = Height / _sourceDimensions.Height;
+      Width = _sourceDimensions.Width * ScaleFactor;
+    }
+    else
+    {
+      Width = _finalSize;
+      ScaleFactor = Width / _sourceDimensions.Width;
+      Height = _sourceDimensions.Height * ScaleFactor;
+    }
+  }
 }
