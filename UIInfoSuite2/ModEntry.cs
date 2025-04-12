6583f4f9ba3b8bba7c901933280b2905b9bb8b59
Drew Hoener <drewhoener@gmail.com>
2025-04-12 14:21:12 -0400

    Apparently I forgot the birthday module enable

--- UIInfoSuite2/ModEntry.cs
+++ UIInfoSuite2/ModEntry.cs
@@ -94,6 +94,7 @@ internal class ModEntry : Mod
     RegisterConfigurable<ConfigurableHudIconPositioning>();
     RegisterPatchable<PatchRenderingMenuContentStep>();
     RegisterBaseModuleSingleton<MenuShortcutDisplay>();
+    RegisterHudModuleSingleton<BirthdayReminderModule>();
     RegisterHudModuleSingleton<ConstructionTrackerModule>();
     RegisterHudModuleSingleton<DailyLuckModule>();
     RegisterHudModuleSingleton<DailyWeatherModule>();
