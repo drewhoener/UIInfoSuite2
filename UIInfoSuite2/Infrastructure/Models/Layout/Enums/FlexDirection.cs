76f072773bfb2ad2fd33fdba3433d447fea3c839
Drew Hoener <drewhoener@gmail.com>
2025-12-29 23:11:49 -0500

    Start building out flex layouts

--- UIInfoSuite2/Infrastructure/Models/Layout/Enums/FlexDirection.cs
+++ UIInfoSuite2/Infrastructure/Models/Layout/Enums/FlexDirection.cs
@@ -1,9 +1,6 @@
 ﻿namespace UIInfoSuite2.Infrastructure.Models.Layout.Enums;
 
-public enum FlexDirection
-{
-  Row,
-  Column,
-  RowReverse,
-  ColumnReverse
-}
+public enum FlexDirection { Row, Column, RowReverse, ColumnReverse }
+public enum JustifyContent { Start, End, Center, SpaceBetween, SpaceAround, SpaceEvenly }
+public enum AlignItems { Start, End, Center, Stretch, Baseline }
+public enum FlexWrap { NoWrap, Wrap, WrapReverse }
