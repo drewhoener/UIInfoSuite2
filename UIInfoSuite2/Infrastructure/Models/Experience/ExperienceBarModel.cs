36646e85f0e231dddbdeb7fd4ea08fcdb8c99809
Drew Hoener <drewhoener@gmail.com>
2026-01-05 01:43:14 -0500

    fixup! Increase exp bar default width

--- UIInfoSuite2/Infrastructure/Models/Experience/ExperienceBarModel.cs
+++ UIInfoSuite2/Infrastructure/Models/Experience/ExperienceBarModel.cs
@@ -19,7 +19,7 @@ internal class ExperienceBarModel : ProgressBar
   private int _skillType = Farmer.farmingSkill;
   private XpThreshold _xpThreshold = new(0, 0, 0);
 
-  public ExperienceBarModel(int maxBarWidth = 300) : base(
+  public ExperienceBarModel(int maxBarWidth = 250) : base(
     TextureHelper.SkillFillColors[Farmer.farmingSkill],
     maxBarWidth
   )
